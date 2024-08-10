2a20fdca2769eceb76dbe30899782e136ed8afc5
n0xgg04 <dulieu.vblc@gmail.com>
2024-08-10 18:06:29 +0700

    add: recoil hoc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,31 +2,29 @@ import { Stack } from 'expo-router';
 import React from 'react';
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import { RecoilRoot } from 'recoil';
 import { TanStackQueryProvider, AppThemeProvider } from '@/shared/provider';
 import * as Sentry from '@sentry/react-native';
 import { useLoadingPackages } from '@/shared/hooks';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { withRecoilRoot } from '@/shared/hocs/recoil';
 
-function RootLayout() {
+const RootLayout = withRecoilRoot(function () {
   const { isLoading } = useLoadingPackages();
   if (isLoading) return null;
 
   return (
     <GestureHandlerRootView>
-      <RecoilRoot>
-        <TanStackQueryProvider>
-          <AppThemeProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
-              <Stack.Screen name="info" />
-            </Stack>
-          </AppThemeProvider>
-        </TanStackQueryProvider>
-      </RecoilRoot>
+      <TanStackQueryProvider>
+        <AppThemeProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+            <Stack.Screen name="info" />
+          </Stack>
+        </AppThemeProvider>
+      </TanStackQueryProvider>
     </GestureHandlerRootView>
   );
-}
+});
 
 export default Sentry.wrap(RootLayout);
